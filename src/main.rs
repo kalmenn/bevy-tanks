31246179dd5a70b84aa7272dba66d4eff10c029f
kalmenn <kalmenn@proton.me>
2023-08-30 01:57:41 +0200

    main: set logging level to debug for debug_assertions builds
    Signed-off-by: kalmenn <kalmenn@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::time::{Duration, Instant};
 
-use bevy::prelude::*;
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
 use leafwing_input_manager::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(InputManagerPlugin::<Action>::default())
+        .add_plugins((
+            DefaultPlugins.set(LogPlugin {
+                level: if cfg!(debug_assertions) {
+                    Level::DEBUG
+                } else {
+                    Level::INFO
+                },
+                ..default()
+            }),
+            InputManagerPlugin::<Action>::default(),
+        ))
         .insert_resource(ShotCooldown(Duration::from_secs(5)))
         .add_systems(Startup, (setup_player, setup_camera))
         .run();
